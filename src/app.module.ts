7d02aa6d95bc52b15523725802d3df999e1ecdf9
caionobrex <marialoucos@hotmail.com.br>
2023-02-11 20:07:23 -0300

    setup pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import AuthModule from './auth/auth.module';
 import ChatModule from './chats/chat.module';
 import { UsersModule } from './users/users.module';
@@ -7,6 +8,16 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
     AuthModule,
     ChatModule,
     UsersModule,
